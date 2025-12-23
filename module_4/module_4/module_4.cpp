f91d385c027d812a177899435aeffb04bd04b639
AlexKlimuk <Lechiy@tut.by>
2025-12-23 17:56:01 +0300

    module_1 weith ignore

---
+++ module_4/module_4/module_4.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <windows.h>
+//#include <sysinfoapi.h>
+//#include <winnt.h>
+
+int main()
+{
+    OSVERSIONINFO osver;
+    osver.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+    GetVersionExW(&osver);
+
+      std::cout << "Windows version: " << osver.dwMajorVersion << std::endl;
+      std::cout << " " << osver.dwMinorVersion << std::endl;
+      std::cout << "Build number:  " << osver.dwBuildNumber << std::endl;
+      std::cout << " " << osver.dwPlatformId << std::endl;
+
+    
+     
+
+}
+
+
+
+
+    
+
+
