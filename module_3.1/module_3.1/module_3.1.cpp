1bede04945a4d73402ecc971ffafb457a9afec0d
AlexKlimuk <Lechiy@tut.by>
2025-12-27 16:29:25 +0300

    module_3.1

---
+++ module_3.1/module_3.1/module_3.1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+enum class TrafficLight { UNKNOWN = -1, RED, YELLOW, GREEN };
+
+void printLight(TrafficLight color) {
+
+    switch (color) {
+        case TrafficLight::RED:
+            std::cout << "Red" << std::endl;
+            break;
+        case TrafficLight::YELLOW:
+            std::cout << "Yellow" << std::endl;
+            break;
+        case TrafficLight::GREEN:
+            std::cout << "Green" << std::endl;
+            break;
+        default:
+            std::cout << "Unknown color" << std::endl;
+    }
+}
+
+int main()
+{
+    TrafficLight light;
+
+    std::string color;
+    std::string strRed = "Red";
+    std::string strYellow = "Yellow";
+    std::string strGreen = "Green";
+    
+    std::cout << "Enter color Red, Yellow, Green: " << std::endl;
+    std::cin >> color;
+    std::transform(color.begin(), color.end(), color.begin(), ::tolower);
+   
+    std::transform(strRed.begin(), strRed.end(), strRed.begin(), ::tolower);
+    std::transform(strYellow.begin(), strYellow.end(), strYellow.begin(), ::tolower);
+    std::transform(strGreen.begin(), strGreen.end(), strGreen.begin(), ::tolower);
+    //std::cout << color << std::endl;
+    
+    if (color == strRed) printLight(TrafficLight::RED);
+    else if (color == strYellow) printLight(TrafficLight::YELLOW);
+    else if (color == strGreen) printLight(TrafficLight::GREEN);
+    else printLight(TrafficLight::UNKNOWN);
+
+}
