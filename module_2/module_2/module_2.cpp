2ade21882355590e9b33df513ea9a1d6bcc1f95c
AlexKlimuk <Lechiy@tut.by>
2025-12-28 23:49:09 +0300

    module_2

--- module_2/module_2/module_2.cpp
+++ module_2/module_2/module_2.cpp
@@ -14,57 +14,55 @@ int main()
 
          //a + b plus
          // a - b minus 
-         // a * b times
-    std::cout << "a + b = " << a + b << "; a - b = " << a - b << "; a * b = " << a * b << std::endl;
+         // a * b Times
+    std::cout << a + b << " " << a - b << " " << a * b;
 
     //a == b equal to
-    if (a == b) { std::cout << "a == b it's " << true << std::endl; }
-    else { std::cout << "a == b it's " << false << std::endl; }
+    if (a == b) { std::cout << true << std::endl; }
+    else { std::cout << false << std::endl; }
 
     //a != b not equal to
-    if (a != b) { std::cout << "a != b it's " << true << std::endl; }
-    else { std::cout << "a != b it's " << false << std::endl; }
+    if (a != b) { std::cout << true << std::endl; }
+    else { std::cout << false << std::endl; }
 
     //a > b more than
-    if (a > b) { std::cout << "a > b it's " << true << std::endl; }
-    else { std::cout << "a > b it's " << false << std::endl; }
+    if (a > b) { std::cout << true << std::endl; }
+    else { std::cout << false << std::endl; }
 
     //a < b  less than
-    if (a < b) { std::cout << "a < b it's " << true << std::endl; }
-    else { std::cout << "a < b it's " << false << std::endl; }
+    if (a < b) { std::cout << true << std::endl; }
+    else { std::cout << false << std::endl; }
 
     //a >= b greater than or equal to
-    if (a <= b) { std::cout << "a <= b it's " << true << std::endl; }
-    else { std::cout << "a <= b it's " << false << std::endl; }
+    if (a <= b) { std::cout << true << std::endl; }
+    else { std::cout << false << std::endl; }
 
     //a <= b less than or equal to
-    if (a >= b) { std::cout << "a >= b it's " << true << std::endl; }
-    else { std::cout << "a >= b it's " << false << std::endl; }
+    if (a >= b) { std::cout << true << std::endl; }
+    else { std::cout << false << std::endl; }
 
     //a & b AND
     int c = a & b;
-    std::cout << "a & b = " << c << std::endl;
+    std::cout << c << std::endl;
 
     //a | b OR
     int d = a | b;
-    std::cout << "a | b = " << d << std::endl;
+    std::cout << d << std::endl;
 
     //a ^ b XOR
     int e = a ^ b;
-    std::cout << "a ^ b = " << e << std::endl;
+    std::cout << d << std::endl;
 
     //~a NOT
     int f = ~a;
-    std::cout << "~a = "<< f << std::endl;
+    std::cout << f << std::endl;
 
     //a << 2
     int g = b << 2;
-    std::cout << "a << 2 = " << g << std::endl;
+    std::cout << g << std::endl;
 
     //b >> 1
     int h = b >> 1;
-    std::cout << "b >> 1 = " << h << std::endl;
-
-
+    std::cout << h << std::endl;
 
 }
