b3048091c2d7583cb5e18d5e36095de6c742129a
AlexKlimuk <Lechiy@tut.by>
2026-01-08 23:26:40 +0300

    create module_5

--- module_5/module_5/module_5.cpp
+++ module_5/module_5/module_5.cpp
@@ -1,20 +1,63 @@
-﻿// module_5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
+#include<string>
+#include <algorithm> // Для std::transform
+#include <cctype>    // Для tolower
+
+
+enum class permissions {
+    UNKNOW = -1,
+    GUEST,
+    USER,
+    ADMIN
+};
+
+void printPermission (permissions people) {
+
+    switch(people) {
+    case permissions::GUEST:
+        std::cout << "this people guest" << std::endl;
+        break;
+
+    case permissions::USER:
+        std::cout << "this people user" << std::endl;
+        break;
+
+    case permissions::ADMIN:
+        std::cout << "this people administrator" << std::endl;
+        break;
+
+    default:
+        std::cout << "unknow this people" << std::endl;
+        break;
+    }
+
+};
+
+permissions selectPeople(std::string people) {
+
+    if ("guest" == people) return permissions::GUEST;
+    else if ("user" == people) return permissions::USER;
+    else if ("admin" == people) return permissions::ADMIN;
+    else return permissions::UNKNOW;
+
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "enter permission" << std::endl;
+
+    std::string people;
+    std::cin >> people;
+
+    std::transform(people.begin(), people.end(), people.begin(), ::tolower);
+
+
+
+    permissions securiti = selectPeople(people);
+
+    //std::cout << people << std::endl;
+    printPermission(securiti);
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
