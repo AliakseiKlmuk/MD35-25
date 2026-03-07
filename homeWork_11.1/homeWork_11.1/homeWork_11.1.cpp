d721e3d35c76466a09ac02a15d00c6e0ce2d9e68
AlexKlimuk <Lechiy@tut.by>
2026-03-07 22:04:25 +0400

    create module_14

--- homeWork_11.1/homeWork_11.1/homeWork_11.1.cpp
+++ homeWork_11.1/homeWork_11.1/homeWork_11.1.cpp
@@ -10,6 +10,8 @@ int main()
     UserDB userDB;
     User user{};
 
+    std::vector <User> _users;
+
     user.id = "0300-30";
     user.name = "Aliaksei";
     user.email = "Lechiy@tut.by";
@@ -17,6 +19,11 @@ int main()
     userDB.addUser(user);
     userDB.print();
 
+    //auto begin_it = _users.begin();
+
+    auto lenght = _users.end() - _users.begin();
+
+    std::cout << lenght << std::endl;
 
     
 
---
+++ module_14/module_14/module_14.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include "ImageFabric.h"
+
+
+int main()
+{
+    int d = 22;
+
+    int a = d;
+
+    auto objectToMove = ImageFabric::createImage(10, 20);
+
+
+    auto img{ std::move(objectToMove) };
+
+    std::cout << "maved: " << std::endl;
+
+    std::cout << "Hello World!\n";
+}
